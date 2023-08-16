8704592c5a78f16cf7602c700ab7032f49464001
Amandeep-singh98 <136148843+Amandeep-singh98@users.noreply.github.com>
2023-08-16 22:06:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react'
+import HomePage from './Component/HomePage';
+// import HomePage2 from './Component/HomePage2';
+function App() {
+
+
+
+
+  
+  
+
+    // const handleClick = (data )=>{
+    //   console.log("dnuefb 3rfbhc3rjfvchj3rvfcjhfr3vhvcf3ehvjd:" , data)
+    // //  setOnSaveData(data)
+    // //  console.log("ev fs",onSaveData)
+    //   // onCancel()
+    //     }
+
+  return (
+    <>
+    <div className=''>
+  {/* { showPopUp===true ? <Popup onCancel={onCancel}  /> : ""} */}
+    <HomePage />
+    {/* <HomePage2 /> */}
+    </div>
+    </>
+  );
+}
+
+export default App;
